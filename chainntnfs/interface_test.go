3ca4697ca5103e2a2ef0b433ea4d32f29827bd30
Harris Brakmic <brakmic@gmail.com>
2019-02-23 22:53:34 +0100

    use actrino

--- chainntnfs/interface_test.go
+++ chainntnfs/interface_test.go
@@ -18,7 +18,7 @@ import (
 	"github.com/Actinium-project/acmutil"
 	"github.com/Actinium-project/acmwallet/chain"
 	_ "github.com/Actinium-project/acmwallet/walletdb/bdb" // Required to auto-register the boltdb walletdb implementation.
-	"github.com/Actinium-project/actrino"
+	neutrino "github.com/Actinium-project/actrino"
 	"github.com/Actinium-project/lnd/chainntnfs"
 	"github.com/Actinium-project/lnd/chainntnfs/bitcoindnotify"
 	"github.com/Actinium-project/lnd/chainntnfs/btcdnotify"
